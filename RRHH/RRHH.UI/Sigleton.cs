3f95122ec0c8fa122e07789c40ec35ece32b0351
Toshiba <Toshiba@TOSHIBA-PC>
2017-08-24 17:08:43 -0600

    Singleton

--- RRHH/RRHH.UI/Sigleton.cs
+++ RRHH/RRHH.UI/Sigleton.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Web;
 
 
+
 namespace RRHH.UI
 {
     public class Sigleton
     {
         public static DS.Interfaces.IEmpleado OpEmpleados = new DS.Metodos.MEmpleado();
+        public static DS.Interfaces.IControlAuditorias opaudi = new DS.Metodos.MControlAuditorias();
+        public static DS.Interfaces.IControlErrores opErrores = new DS.Metodos.MControlErrores();
+        public static DS.Interfaces.Idepartamento opdepartamento = new DS.Metodos.MDepartamento();
+        public static DS.Interfaces.IIncapacidad opIncapacidad = new DS.Metodos.MIncapacidad();
+        public static DS.Interfaces.IRoles oproles = new DS.Metodos.MRoles();
+        public static DS.Interfaces.ISolicitudVacaciones opsolicitud = new DS.Metodos.MSolicitudVacaciones();
+
     }
 }
